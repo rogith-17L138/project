76a99642bf6c54dfe5f380ef165dea8b3c9f33c4
rogith-17L138 <68704985+rogith-17L138@users.noreply.github.com>
2020-12-19 16:32:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
 from flask import Flask , render_template ,request
+from flask_mysqldb import MySQL
 import smtplib
-import sqlite3
-con=sqlite3.connect('pro.db',check_same_thread=False)
-cur=con.cursor()
-#cur.execute('CREATE TABLE Arduino(NAME VARCHAR(20),GENDER VARCHAR(20),EMAIL VARCHAR(20) PRIMARY KEY,NUM INT ,CLG VARCHAR(20),YEAR VARCHAR(20))')
-#cur.execute('CREATE TABLE Cloud(NAME VARCHAR(20),GENDER VARCHAR(20),EMAIL VARCHAR(20) PRIMARY KEY,NUM INT ,CLG VARCHAR(20),YEAR VARCHAR(20))')
-#cur.execute('CREATE TABLE Cyber(NAME VARCHAR(20),GENDER VARCHAR(20),EMAIL VARCHAR(20) PRIMARY KEY,NUM INT ,CLG VARCHAR(20),YEAR VARCHAR(20))')
-#cur.execute('CREATE TABLE Flutter(NAME VARCHAR(20),GENDER VARCHAR(20),EMAIL VARCHAR(20) PRIMARY KEY,NUM INT ,CLG VARCHAR(20),YEAR VARCHAR(20))')
-#cur.execute('CREATE TABLE IOT(NAME VARCHAR(20),GENDER VARCHAR(20),EMAIL VARCHAR(20) PRIMARY KEY,NUM INT ,CLG VARCHAR(20),YEAR VARCHAR(20))')
-#cur.execute('CREATE TABLE PCB(NAME VARCHAR(20),GENDER VARCHAR(20),EMAIL VARCHAR(20) PRIMARY KEY,NUM INT ,CLG VARCHAR(20),YEAR VARCHAR(20))')
 app=Flask(__name__)
+app.config['MYSQL_USER']='sql12378478'
+app.config['MYSQL_PASSWORD']='FHkjlJkAeP'
+app.config['MYSQL_HOST']='sql12.freemysqlhosting.net'
+app.config['MYSQL_DB']='sql12378478'
+app.config['MYSQL_CURSORCLASS']='DictCursor'
+mysql=MySQL(app)
+
 @app.route('/')
-@app.route('/home.html')
 def index():
+    # cur = mysql.connection.cursor()
+    # print("Database opened successfully")
+    # cur.execute('CREATE TABLE Arduino(NAME VARCHAR(20),GENDER VARCHAR(20),EMAIL VARCHAR(20) PRIMARY KEY,NUM INT  ,CLG VARCHAR(20),YEAR VARCHAR(20))')
+    # cur.execute('CREATE TABLE Cloud(NAME VARCHAR(20),GENDER VARCHAR(20),EMAIL VARCHAR(20) PRIMARY KEY,NUM INT ,CLG VARCHAR(20),YEAR VARCHAR(20))')
+    # cur.execute('CREATE TABLE Cyber(NAME VARCHAR(20),GENDER VARCHAR(20),EMAIL VARCHAR(20) PRIMARY KEY,NUM INT ,CLG VARCHAR(20),YEAR VARCHAR(20))')
+    # cur.execute('CREATE TABLE Flutter(NAME VARCHAR(20),GENDER VARCHAR(20),EMAIL VARCHAR(20) PRIMARY KEY,NUM INT ,CLG VARCHAR(20),YEAR VARCHAR(20))')
+    # cur.execute('CREATE TABLE IOT(NAME VARCHAR(20),GENDER VARCHAR(20),EMAIL VARCHAR(20) PRIMARY KEY,NUM INT ,CLG VARCHAR(20),YEAR VARCHAR(20))')
+    # cur.execute('CREATE TABLE PCB(NAME VARCHAR(20),GENDER VARCHAR(20),EMAIL VARCHAR(20) PRIMARY KEY,NUM INT ,CLG VARCHAR(20),YEAR VARCHAR(20))')
+    # print("Table created successfully")
+    # mysql.connection.commit()
+    # cur.close()
+    return render_template('home.html')
+@app.route('/home.html')
+def home():
     return render_template('home.html')
 @app.route('/arduino.html')
 def arduino():
@@ -35,7 +47,7 @@ def pcb():
 @app.route('/reg.html',methods=['POST','GET'])
 def reg():
     if request.method=='POST':
-        try:
+        if 1:
             d={}
             d['name']=request.form['name']
             d['gender']=request.form['gender']
@@ -44,29 +56,31 @@ def reg():
             d['clg']=request.form['clg']
             d['year']=request.form['year']
             d['web']=request.form['web']
+            #cur = mysql.connection.cursor()
             if d['web']=="Arduino":
-                cur.execute('INSERT INTO Arduino(NAME,GENDER,EMAIL,NUM,CLG,YEAR) VALUES(?,?,?,?,?,?)',(d['name'],d['gender'],d['email'],d['num'],d['clg'],d['year']))
+                cur.execute("INSERT into Arduino(NAME,GENDER,EMAIL,NUM,CLG,YEAR)values(%s,%s,%s,%s,%s,%s)",(d['name'],d['gender'],d['email'],d['num'],d['clg'],d['year']))
             elif d['web']=="Cloud Computing":
-                cur.execute('INSERT INTO Cloud(NAME,GENDER,EMAIL,NUM,CLG,YEAR) VALUES(?,?,?,?,?,?)',(d['name'],d['gender'],d['email'],d['num'],d['clg'],d['year']))
+                cur.execute("INSERT INTO Cloud(NAME,GENDER,EMAIL,NUM,CLG,YEAR)VALUES(%s,%s,%s,%s,%s,%s)",(d['name'],d['gender'],d['email'],d['num'],d['clg'],d['year']))
             elif d['web']=="Cyber Security":
-                cur.execute('INSERT INTO Cyber(NAME,GENDER,EMAIL,NUM,CLG,YEAR) VALUES(?,?,?,?,?,?)',(d['name'],d['gender'],d['email'],d['num'],d['clg'],d['year']))
+                cur.execute("INSERT INTO Cyber(NAME,GENDER,EMAIL,NUM,CLG,YEAR)VALUES(%s,%s,%s,%s,%s,%s)",(d['name'],d['gender'],d['email'],d['num'],d['clg'],d['year']))
             elif d['web']=="Flutter":
-                cur.execute('INSERT INTO Flutter(NAME,GENDER,EMAIL,NUM,CLG,YEAR) VALUES(?,?,?,?,?,?)',(d['name'],d['gender'],d['email'],d['num'],d['clg'],d['year']))
+                print('hi')
+                #cur.execute("INSERT INTO Flutter(NAME,GENDER,EMAIL,NUM,CLG,YEAR)VALUES(%s,%s,%s,%s,%s,%s)",(d['name'],d['gender'],d['email'],d['num'],d['clg'],d['year']))
             elif d['web']=="IOT":
-                cur.execute('INSERT INTO IOT(NAME,GENDER,EMAIL,NUM,CLG,YEAR) VALUES(?,?,?,?,?,?)',(d['name'],d['gender'],d['email'],d['num'],d['clg'],d['year']))
+                cur.execute("INSERT INTO IOT(NAME,GENDER,EMAIL,NUM,CLG,YEAR)VALUES(%s,%s,%s,%s,%s,%s)",(d['name'],d['gender'],d['email'],d['num'],d['clg'],d['year']))
             elif d['web']=="PCB":
-                cur.execute('INSERT INTO PCB(NAME,GENDER,EMAIL,NUM,CLG,YEAR) VALUES(?,?,?,?,?,?)',(d['name'],d['gender'],d['email'],d['num'],d['clg'],d['year'])) 
-            con.commit()
+                cur.execute("INSERT INTO PCB(NAME,GENDER,EMAIL,NUM,CLG,YEAR)VALUES(%s,%s,%s,%s,%s,%s)",(d['name'],d['gender'],d['email'],d['num'],d['clg'],d['year']))
+            #mysql.connection.commit()
             email_server=smtplib.SMTP("smtp.gmail.com",587)
             email_server.starttls()
             SUBJECT="Webinar registration"
             mail="rogopi42@gmail.com"
-            
+
             TEXT="HI "+(d['name']).upper()+"\n           Greetings from RoGopi Software Solutions Webinar team! Thanks for your interest towards our webinar programme.Your registration for "+d['web']+" webinar is successful.The webinar link will be provided on the date of the webinar and the e-certificate will be provided at the end of the event.For any queries write to rogopi42@gmail.com"
             #msg='Subject: {}\n\n{}'.format(SUBJECT,TEXT)
             #msg="FROM : From RoGopi {} \n\n Subject : Webinar Registration \n\n {}".format('rogopi42@gmail.com',TEXT)
             msg=""" FROM : RoGopi %s\n"""%('Software Solution')
-            ms0="""TO : %s\n"""%(d['email'])
+            ms0="""TO : %s %s\n"""%(d['name'],d['email'])
             msg=msg+ms0
             ms1="""Subject : Webinar Registration
 
@@ -78,8 +92,9 @@ def reg():
             email_server.sendmail(mail,d['email'],msg)
             email_server.quit()
             return render_template('end.html')
-        except Exception as e:
-            return render_template('error.html')
+        '''except Exception as e:
+            print("dd",e.args)
+            return render_template('error.html')'''
     else:
         return render_template('reg.html')
 @app.route('/end.html')
@@ -89,4 +104,4 @@ def end():
 def error():
     return render_template('error.html')
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run()
